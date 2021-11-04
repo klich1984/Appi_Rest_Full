a81d6dade8a8a1c34db6953d77a74b25c556726f
Carlos Usuga Martinez <klich84@gmail.com>
2021-11-03 20:19:07 -0500

    add file html with view to use

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express'),
+	cors = require('cors')
 
 // Instance express
 const app = express()
@@ -6,11 +7,12 @@ const app = express()
 // Settings
 app.set('port', process.env.PORT || 3000)
 app.use(express.json())
+app.use(cors())
 
 // Routes
 app.use(require('./routes/articles'))
 
 // Starting the server
 app.listen(app.get('port'), () => {
-  console.log('Server Running in port:', app.get('port'))
+	console.log('Server Running in port:', app.get('port'))
 })
